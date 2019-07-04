aeaf822e121a4ff4aff0f990cde86415a7b1e021
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-07-04 16:40:18 +0900

    백준 1316 수정

--- Algorithm_study/백준_~1학기방학/백준 1316.py
+++ Algorithm_study/백준_~1학기방학/백준 1316.py
@@ -1,21 +1,18 @@
-#1316번
-counts = 0
-i = int(input())
-for i in range(i):
-    r = input()
-    for ii in range(len(r)):
-        if r.count(r[ii])>1:
-            if ii != len(r)-1:
-                if r[ii] == r[ii+1]:
-                    continue
-                else:
-                    if r[ii] == r[ii-1]:
-                        continue
-                    else:
-                        break
-        else:
-            if ii == (len(r)-1):
-                counts += 1
-            else:
-                continue
-print(counts)
+#백준 1316
+def check(voca):
+    for i in range(len(voca)):
+        judge = ['F','F']
+        for j in range(i+1,len(voca)):
+            if voca[i] != voca[j]:
+                judge[0] = 'T'
+            if voca[i] == voca[j] and judge[0] == 'T':
+                judge[1] = 'T'
+            if judge[0] == 'T' and judge[1] == 'T':
+                return 0
+    return 1
+N = int(input())
+cnt = 0
+for k in range(N):
+    cnt += check(input())
+print(cnt)
+
