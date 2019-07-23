3345b622c8004f7896ee5b7856b118b1ac2c4ab1
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-07-23 21:10:01 +0900

    백준 4948
    수정

--- Algorithm_study/백준_~1학기방학/백준 4948.py
+++ Algorithm_study/백준_~1학기방학/백준 4948.py
@@ -1,18 +1,19 @@
 #4948
-while True:
-    num_count = 0
-    n = int(input())
-    if n != 0:
-        for i in range(n+1,n*2+1):
-            check = True
-            if i != 1:
-                for j in range(2,i):
-                    if i % j == 0:
-                        check = False
-            else:
-                check = False
-            if check == True:
+def _4948():
+    b = [False,False] + [True]*250000
+    for i in range(2,250000):
+        if b[i]:
+            for j in range(2*i,250000,i):
+                b[j] = False
+
+    while True:
+        num_count = 0
+        n = int(input())
+        if n == 0:
+            exit()
+        for n in range(n+1,n*2+1):
+            if b[n]:
                 num_count += 1
         print(num_count)
-    else:
-        exit()
+
+_4948()
