28f8155517db5f6a51a6b66a8ff25675260ed1a7
BCSJH <39579101+BCSJH@users.noreply.github.com>
2019-07-24 23:07:22 +0900

    백준 1037
    틀림 수정

--- Algorithm_study/백준_~1학기방학/백준 1037.py
+++ Algorithm_study/백준_~1학기방학/백준 1037.py
@@ -1,8 +1,16 @@
 #1037
 
 n = int(input())
-maxs = 1
+mins = 0
+maxs = 0
 ns = list(map(int,input().split()))
 for i in range(len(ns)):
-    maxs *= ns[i]
-print(maxs)
+    if i: # i가 0일 때
+        if ns[i] > maxs:
+            maxs = ns[i]
+        if ns[i] < mins:
+            mins = ns[i]
+    else:
+        maxs = ns[i]
+        mins = ns[i]
+print(maxs * mins)
